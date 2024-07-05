c053d770a315210eb62aa5945254a2f353ed93e4
ArazAbedini <araz.abedini1381@gmail.com>
2024-07-05 18:27:56 +0330

    init commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/hotel', {
+    useNewUrlParser:true,
+})
---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require('./db/mongoose');
+const userRouter = require('./router/user')
+// const User = require('./models/user');
+const app = express()
+
+
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(userRouter)
+
+
+
+
+app.listen(port, () => {
+    console.log('server is UP!')
+})
